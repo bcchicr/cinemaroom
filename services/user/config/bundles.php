0916b2e14fecb32145567c8804d73e802a3cb69e
bcchicr <work.bcchicr@gmail.com>
2025-10-11 23:53:07 +0300

    chore(user): added trap and buggregator support
    Refs #20

--- services/user/config/bundles.php
+++ services/user/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Baldinof\RoadRunnerBundle\BaldinofRoadRunnerBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['dev' => true],
 ];
