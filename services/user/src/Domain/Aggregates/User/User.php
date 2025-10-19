ea48ff96aa36704a2224f237647184a757a04226
bcchicr <work.bcchicr@gmail.com>
2025-10-19 06:53:59 +0300

    chore(user): check commiting of formatted code

--- services/user/src/Domain/Aggregates/User/User.php
+++ services/user/src/Domain/Aggregates/User/User.php
@@ -9,6 +9,10 @@ use App\Domain\Events\UserAvatarChanged;
 use App\Domain\Events\UserBioChanged;
 use App\Domain\Events\UserRegistered;
 use App\Domain\Events\UserUsernameChanged;
+
+
+
+use App\Domain\Exceptions\FailedInvariantException;
 use App\Domain\Exceptions\InvalidArgumentException;
 use App\Domain\ValueObjects\Avatar;
 use App\Domain\ValueObjects\UserId;
